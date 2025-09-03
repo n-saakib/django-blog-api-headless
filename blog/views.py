19fa0ab4357ebe132de514b67fac50f4672426b7
Nazmus Sakib <nsakib800@gmail.com>
2025-09-03 06:53:15 +0600

    Adds and registers blog models and make migrations

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,6 @@ from django.http import JsonResponse
 from blog.schemas import MessageResponse
 
 
-def hello_api(request):
+def hello_api(request) -> JsonResponse:
     response = MessageResponse(message="Hello, World!")
     return JsonResponse(response.model_dump())
